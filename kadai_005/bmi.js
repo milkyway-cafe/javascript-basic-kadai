b7d4f3db030031621008962565ea3d9f4a534cf4
milkyway-cafe <rainbowpastelflower@gmail.com>
2025-10-16 15:31:42 +0900

    kadai_005アップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,18 @@
+let bodyWeight;
+
+bodyWeight = 68;
+
+let height;
+
+height = 1.7;
+
+let squreHeight;
+
+squreHeight = ( height * height );
+
+let bmi;
+
+bmi = ( bodyWeight / squreHeight );
+
+console.log( bmi );
+
