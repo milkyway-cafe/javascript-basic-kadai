2f3a900449d908f1414398871f202a2a57c44052
milkyway-cafe <rainbowpastelflower@gmail.com>
2025-10-17 08:59:30 +0900

    Update holiday.js
    訂正

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,15 +1,16 @@
-const holiday = ["正月", "成人の日", "建国記念日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "敬老感謝の日"]
+const holidays = ["正月", "成人の日", "建国記念の日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"]
 
 
-for (let i = 0; i <= 15; i += 1){
-  console.log(holiday[i]);
+for (let i = 0; i <= holidays.length; i += 1){
+  console.log(holidays[i]);
 }
 
 
 // while を考える。前半はあってる。
 
 let i = 0;
-while ( i < holiday.length) {
-  console.log(holiday[ i ]);
+while ( i < holidays.length) {
+  console.log(holidas[ i ]);
   i++;
 }
+
